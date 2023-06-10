08e5ce9dcfe41664451a2fb84dad806fce718b06
soto <josesoto1993@gmail.com>
2023-06-10 13:57:45 -0300

    handle pax schedule in the module itself

--- engines/pax_schedule/max_money.py
+++ engines/pax_schedule/max_money.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import random
 import time
@@ -19,27 +20,92 @@ POPUP_SELECT_LETS_GO_HOVER = "data/engine_schedule/popup_engine_schedule_lests_g
 logging.basicConfig(level=logging.INFO)
 
 
-def set_schedule():
-    try:
+def select_pax_engine(sleep_select_pax_engine=10):
+    find_image_and_click([PAX_ENGINE_HEADER],
+                         on_screen_msg="Select pax engine",
+                         on_fail_msg="Fail select pax engine")
+    sleep_random(sleep_select_pax_engine)
+
+
+def open_timetable(sleep_timetable=10):
+    find_image_and_click([POPUP_TIMETABLE_BASE, POPUP_TIMETABLE_HOVER],
+                         on_screen_msg="Open timetable",
+                         on_fail_msg="Fail open timetable")
+    sleep_random(sleep_timetable)
+
+
+def open_adopt_schedule(sleep_adopt_schedule=20):
+    find_image_and_click([POPUP_TIMETABLE_ADOPT_SCHEDULE_BASE, POPUP_TIMETABLE_ADOPT_SCHEDULE_HOVER],
+                         on_screen_msg="Open adopt schedule",
+                         on_fail_msg="Fail open adopt schedule")
+    sleep_random(sleep_adopt_schedule)
+
+
+def select_all_engines(sleep_select_all=30):
+    # TODO: use the function, not the hardcoded
+    # find_image_and_click([POPUP_SELECT_ALL_BASE, POPUP_SELECT_ALL_HOVER],
+    #                       on_screen_msg="Select all engines",
+    #                       on_fail_msg="Fail select all engines",
+    #                       precision=0.8)
+    click_on_rect_area(top_left_corner=(250, 500), size=(25, 25))
+    sleep_random(sleep_select_all)
+    # ENDTODO
+
+
+def select_lets_go(sleep_lets_go=30):
+    find_image_and_click([POPUP_SELECT_LETS_GO_BASE, POPUP_SELECT_LETS_GO_HOVER],
+                         on_screen_msg="Select lets go",
+                         on_fail_msg="Fail select lets go")
+    sleep_random(sleep_lets_go)
+
+
+def sleep_random(sleep_time):
+    time.sleep(random.uniform(sleep_time, sleep_time * 1.5))
+
+
+class PaxSchedule:
+    def __init__(self, start_minute=5):
+        self.next_run_time = datetime.datetime.now()
+        self.start_minute = start_minute
+        self.sleep_select_pax_engine = 10
+        self.sleep_timetable = 10
+        self.sleep_adopt_schedule = 20
+        self.sleep_select_all = 30
+        self.sleep_lets_go = 30
+
+    def set_schedule(self):
+        if self.should_run():
+            try:
+                self.run_pax_engine_schedule()
+                self.update_next_run_time()
+            except Exception as exception:
+                logging.error(str(exception))
+                return
+
+    def run_pax_engine_schedule(self):
+        logging.info(f"----- Run pax engine schedule: Start -----")
         open_tab(Tabs.ENGINES)
-        find_image_and_click([PAX_ENGINE_HEADER],
-                             on_screen_msg="Open pax engine",
-                             on_fail_msg="Fail open pax engine")
-        find_image_and_click([POPUP_TIMETABLE_BASE, POPUP_TIMETABLE_HOVER],
-                             on_screen_msg="Open timetable",
-                             on_fail_msg="Fail open timetable")
-        find_image_and_click([POPUP_TIMETABLE_ADOPT_SCHEDULE_BASE, POPUP_TIMETABLE_ADOPT_SCHEDULE_HOVER],
-                             on_screen_msg="Open adopt schedule",
-                             on_fail_msg="Fail open adopt schedule")
-        # TODO: use the function, not the hardcoded
-        # find_image_and_click([POPUP_SELECT_ALL_BASE, POPUP_SELECT_ALL_HOVER], on_screen_msg="Select all engines", on_fail_msg="Fail select all engines", precision=0.8)
-        time.sleep(random.uniform(20, 30))
-        click_on_rect_area(top_left_corner=(250, 500), size=(25, 25))
-        time.sleep(random.uniform(30, 45))
-        # ENDTODO
-        find_image_and_click([POPUP_SELECT_LETS_GO_BASE, POPUP_SELECT_LETS_GO_HOVER],
-                             on_screen_msg="Select lets go",
-                             on_fail_msg="Fail select lets go")
-    except Exception as exception:
-        logging.error(str(exception))
-        return
+        select_pax_engine(self.sleep_select_pax_engine)
+        open_timetable(self.sleep_timetable)
+        open_adopt_schedule(self.sleep_adopt_schedule)
+        select_all_engines(self.sleep_select_all)
+        select_lets_go(self.sleep_lets_go)
+
+    def update_next_run_time(self):
+        current_datetime = datetime.datetime.now()
+        target_hour = (current_datetime.hour + 1) % 24
+        target_minute = random.randint(self.start_minute, self.start_minute + 10)
+        if target_minute >= 60:
+            target_minute = target_minute - 60
+            target_hour = (target_hour + 1) % 24
+
+        target_datetime = current_datetime.replace(hour=target_hour, minute=target_minute)
+
+        if target_datetime < current_datetime:
+            target_datetime += datetime.timedelta(days=1)
+
+        self.next_run_time = target_datetime
+        logging.info(f"----- Next run at {target_datetime.time()} -----")
+
+    def should_run(self):
+        return datetime.datetime.now() >= self.next_run_time
--- main.py
+++ main.py
@@ -1,54 +1,33 @@
-import datetime
 import logging
-import random
 import time
 
-from engines.pax_schedule import max_money as pax_schedule_max_money
+from engines.pax_schedule.max_money import PaxSchedule
 from rail_utils.rail_utils import get_screenshot
+
+MAIN_LOOP_TIME = 60
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    pax_schedule = PaxSchedule(start_minute=5)
     get_screenshot(save=True)
-    print("Program started.")
-    main_loop()
+    logging.info("Program started.")
+    main_loop(pax_schedule)
 
 
-def main_loop():
+def main_loop(pax_schedule: PaxSchedule):
     while True:
-        try:
-            run_pax_schedule()
-            target_time = get_next_target_time(start_minute=5)
-            sleep_till_target_time(target_time)
-        except Exception as exception:
-            logging.error(str(exception))
+        run_pax_schedule(pax_schedule)
+
+        time.sleep(MAIN_LOOP_TIME)
 
 
-def run_pax_schedule():
-    pax_schedule_max_money.set_schedule()
-
-
-def get_next_target_time(start_minute=5):
-    current_datetime = datetime.datetime.now()
-    target_hour = (current_datetime.hour + 1) % 24
-    target_minute = random.randint(start_minute, start_minute + 10)
-    target_datetime = current_datetime.replace(hour=target_hour, minute=target_minute)
-
-    if target_datetime < current_datetime:
-        target_datetime += datetime.timedelta(days=1)
-
-    print(f"Next run at {target_datetime.time()}")
-
-    return target_datetime
-
-
-def sleep_till_target_time(target_time):
-    remaining_time = target_time - datetime.datetime.now()
-    remaining_seconds = remaining_time.total_seconds()
-    print(f"Sleeping {remaining_seconds}'")
-    time.sleep(remaining_seconds)
-    print(f"----- Run Start -----")
+def run_pax_schedule(pax_schedule: PaxSchedule):
+    try:
+        pax_schedule.set_schedule()
+    except Exception as exception:
+        logging.error(str(exception))
 
 
 if __name__ == "__main__":
