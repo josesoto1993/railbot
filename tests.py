5b0923f5d9825279756f01fa3d4ab17f55276849
soto <josesoto1993@gmail.com>
2023-06-10 23:58:58 -0300

    change test to be more customizable

--- tests.py
+++ tests.py
@@ -1,17 +1,23 @@
 import logging
 import unittest
 
+import pyautogui
+
+from invest.industry_invest.industry_invest import RANKING_SHOW_MORE
 from rail_utils.rail_utils import get_screenshot, image_on_screen
 
-logging.basicConfig(level=logging.DEBUG)
+logging.root.setLevel(logging.DEBUG)
 
 
 class IsImageOnScreen(unittest.TestCase):
     def test_test_image(self):
-        file_path = 'data/tabs_status/ranking_on_load.png'
+        file_path = RANKING_SHOW_MORE
         screenshot = get_screenshot()
-        precision = 0.9
-        is_on_screen, position = image_on_screen(file_path, precision=precision, screenshot=screenshot)
+        screenshot.save("data/screenshot.png")
+        precision = 0.1
+        is_on_screen, position = image_on_screen(file_path, precision=precision, screenshot=screenshot, gray_scale=True)
+        x, y = position
+        pyautogui.moveTo(x=x, y=y)
 
         self.assertTrue(is_on_screen)
 
