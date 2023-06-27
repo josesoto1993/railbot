60d808fa2955b1f0ab621786c8cabd6454681750
Jose Soto <josesoto1993@example.com>
2023-06-27 20:41:56 -0300

    add workers: p/h[s], service [m+s]

--- association/worker_bid/workers.py
+++ association/worker_bid/workers.py
@@ -1,18 +1,34 @@
 WORKER_ACCELERATION = "data/workers/worker_acceleration.png"
 WORKER_ACCELERATION_SMALL = "data/workers/worker_acceleration_small.png"
 WORKER_BUILD_DISCOUNT = "data/workers/worker_build_discount.png"
+# TODO: WORKER_BUILD_DISCOUNT_SMALL
 WORKER_CITY_CONNECTOR = "data/workers/worker_city_connector.png"
+# TODO: WORKER_CITY_CONNECTOR SMALL
 WORKER_COMPETITION = "data/workers/worker_competition.png"
+# TODO: WORKER_COMPETITION SMALL
 WORKER_FASTER_BONUS = "data/workers/worker_faster_bonus.png"
+# TODO: WORKER_FASTER_BONUS SMALL
 WORKER_FASTER_CONSTRUCTION = "data/workers/worker_faster_construction.png"
+# TODO: WORKER_FASTER_CONSTRUCTION SMALL
 WORKER_GOLD_DISCOUNT = "data/workers/worker_gold_discount.png"
 WORKER_GOLD_DISCOUNT_SMALL = "data/workers/worker_gold_discount_small.png"
 WORKER_PAX = "data/workers/worker_pax.png"
+# TODO: WORKER_PAX SMALL
 WORKER_PRESIDENT = "data/workers/worker_president.png"
+# TODO: WORKER_PRESIDENT SMALL
+WORKER_PRESTIGE_HOURLY = "data/workers/worker_prestige_hourly.png"
+WORKER_PRESTIGE_HOURLY_SMALL = "data/workers/worker_prestige_hourly_small.png"
 WORKER_PRESTIGE_INDUSTRY = "data/workers/worker_prestige_industry.png"
+# TODO: WORKER_PRESTIGE_INDUSTRY SMALL
 WORKER_REDUCE_WT = "data/workers/worker_reduce_wt.png"
+# TODO: WORKER_REDUCE_WT SMALL
 WORKER_SCIENCE = "data/workers/worker_science.png"
+# TODO: WORKER_SCIENCE SMALL
+WORKER_SERVICE_DISCOUNT = "data/workers/worker_service_discount.png"
+WORKER_SERVICE_DISCOUNT_SMALL = "data/workers/worker_service_discount_small.png"
 WORKER_SPEED = "data/workers/worker_speed.png"
+# TODO: WORKER_SPEED SMALL
 WORKER_TRACK_DISCOUNT = "data/workers/worker_track_discount.png"
 WORKER_TRACK_DISCOUNT_SMALL = "data/workers/worker_track_discount_small.png"
 WORKER_WAGGONS_DISCOUNT = "data/workers/worker_waggons_discount.png"
+# TODO: WORKER_WAGGONS_DISCOUNT SMALL
