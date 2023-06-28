f4c8bc830c7ae6f9be332978c06c65b43236caf9
Jose Soto <josesoto1993@example.com>
2023-06-28 19:48:24 -0300

    add small: city invest

--- invest/city_invest/city_invest.py
+++ invest/city_invest/city_invest.py
@@ -1,11 +1,6 @@
 import datetime
-import logging
 
-import pyautogui
-
-from rail_utils.rail_utils import sleep_random, find_image_and_click, move_mouse_close_to_center, image_on_screen, \
-    click_on_rect_area, close_all_pop_ups, get_screenshot, ImageNotFoundException, get_image_size, GENERAL_BTN_X_CLOSE, \
-    get_screenshot_with_black_out_of_box
+from rail_utils.rail_utils import *
 from rail_utils.tabs_enum import Tabs
 from rail_utils.tabs_util import open_tab
 
@@ -14,29 +9,46 @@ CITY_HEIGHT = 80
 CITY_WIDTH = 120
 HEIGHT_OFFSET = 70
 INVEST_MINUTES_TO_RECHECK = 180
-MAP_BTN_CENTER = "data/map/btn_center.png"
-BTN_ZOOM_IN_BASE = "data/map/btn_zoom_in_base.png"
-BTN_ZOOM_IN_DISABLED = "data/map/btn_zoom_in_disabled.png"
-CITY_SUBTAB_CITY_PROJECT = "data/city/city_subtab_city_project.png"
-CITY_LABEL_LEFT = "data/city/city_label_left.png"
-CITY_LABEL_RIGHT = "data/city/city_label_right.png"
-USER_LABEL = "data/city/user_donation_label.png"
-CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE = "data/city/city_subtab_city_project_contribute.png"
-CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER = "data/city/city_subtab_city_project_contribute_header.png"
-CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN = "data/city/city_subtab_city_project_contribute_subbtn.png"
+
+MAP_FOLDER = "data/map"
+MAP_BTN_CENTER = MAP_FOLDER + "/btn_center.png"
+MAP_BTN_CENTER_SMALL = MAP_FOLDER + "/btn_center_small.png"
+BTN_ZOOM_IN_BASE = MAP_FOLDER + "/btn_zoom_in_base.png"
+BTN_ZOOM_IN_BASE_SMALL = MAP_FOLDER + "/btn_zoom_in_base_small.png"
+
+CITY_FOLDER = "data/city"
+CITY_SUBTAB_CITY_PROJECT = CITY_FOLDER + "/city_subtab_city_project.png"
+CITY_SUBTAB_CITY_PROJECT_SMALL = CITY_FOLDER + "/city_subtab_city_project_small.png"
+CITY_LABEL_LEFT = CITY_FOLDER + "/city_label_left.png"
+CITY_LABEL_LEFT_SMALL = CITY_FOLDER + "/city_label_left_small.png"
+CITY_LABEL_RIGHT = CITY_FOLDER + "/city_label_right.png"
+CITY_LABEL_RIGHT_SMALL = CITY_FOLDER + "/city_label_right_small.png"
+USER_LABEL = CITY_FOLDER + "/user_donation_label.png"
+USER_LABEL_SMALL = CITY_FOLDER + "/user_donation_label_small.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE = CITY_FOLDER + "/city_subtab_city_project_contribute.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SMALL = CITY_FOLDER + "/city_subtab_city_project_contribute_small.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER = CITY_FOLDER + "/city_subtab_city_project_contribute_header.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER_SMALL = CITY_FOLDER + "/city_subtab_city_project_contribute_header_small.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN = CITY_FOLDER + "/city_subtab_city_project_contribute_subbtn.png"
+CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN_SMALL = CITY_FOLDER + "/city_subtab_city_project_contribute_subbtn_small.png"
+
+TEMP_CITY_LABEL = "start_city_label.png"
+# TODO: Add small version of images
 
 logging.basicConfig(level=logging.INFO)
 
 
 def get_city_label():
-    left_on_screen, left_position, _ = image_on_screen(CITY_LABEL_LEFT)
-    right_on_screen, right_position, _ = image_on_screen(CITY_LABEL_RIGHT)
+    left_city = [CITY_LABEL_LEFT, CITY_LABEL_LEFT_SMALL]
+    left_on_screen, left_position, _, left_image = any_image_on_screen(left_city)
+    right_city = [CITY_LABEL_RIGHT, CITY_LABEL_RIGHT_SMALL]
+    right_on_screen, right_position, _, right_image = image_on_screen(right_city)
 
     if not left_on_screen and not right_on_screen:
         raise ImageNotFoundException(f"Fail cet city label.")
 
-    left_width, left_height = get_image_size(CITY_LABEL_LEFT)
-    right_width, right_height = get_image_size(CITY_LABEL_RIGHT)
+    left_width, left_height = get_image_size(left_image)
+    right_width, right_height = get_image_size(right_image)
 
     x_start = left_position[0] + left_width
     y_start = left_position[1]
@@ -52,10 +64,11 @@ def get_city_label():
 
 def get_screenshot_contribute_pop_up():
     screenshot = get_screenshot()
-    on_screen, position, _ = image_on_screen(CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER, screenshot=screenshot)
+    contribute_header = [CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER, CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER_SMALL]
+    on_screen, position, _, _ = image_on_screen(contribute_header, screenshot=screenshot)
     if not on_screen:
         raise ImageNotFoundException(
-            f"Failed to find header on screen: {CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_HEADER}")
+            f"Failed to find header on screen for city project")
     top_left_corner = (0, position[1])
     screenshot_width, screenshot_height = screenshot.size
     size = (screenshot_width, screenshot_height - position[1])
@@ -97,17 +110,18 @@ class CityInvest:
         close_all_pop_ups()
 
         precision = 0.95
-        on_screen, position, _ = image_on_screen(BTN_ZOOM_IN_BASE, precision=precision)
+        zoom_in_btn = [BTN_ZOOM_IN_BASE, BTN_ZOOM_IN_BASE_SMALL]
+        on_screen, position, _, _ = any_image_on_screen(zoom_in_btn, precision=precision)
         tries = 0
         while on_screen and tries < MAX_ZOOM_CLICKS:
             tries += 1
-            find_image_and_click([BTN_ZOOM_IN_BASE], msg="zoom")
+            find_image_and_click(zoom_in_btn, msg="zoom")
             sleep_random(self.sleep_zoom / 2)
             move_mouse_close_to_center()
             sleep_random(self.sleep_zoom / 2)
-            on_screen, position, _ = image_on_screen(BTN_ZOOM_IN_BASE, precision=precision)
+            on_screen, position, _, _ = any_image_on_screen(zoom_in_btn, precision=precision)
 
-        find_image_and_click([MAP_BTN_CENTER], msg="center map")
+        find_image_and_click([MAP_BTN_CENTER, MAP_BTN_CENTER_SMALL], msg="center map")
         sleep_random(self.sleep_center_city)
 
     def _select_city(self):
@@ -119,12 +133,13 @@ class CityInvest:
         sleep_random(self.sleep_select_city)
 
     def _select_subtab_city_project(self):
-        find_image_and_click([CITY_SUBTAB_CITY_PROJECT], msg="subtab city project")
+        city_project_subtab_btn = [CITY_SUBTAB_CITY_PROJECT, CITY_SUBTAB_CITY_PROJECT_SMALL]
+        find_image_and_click(city_project_subtab_btn, msg="subtab city project")
         sleep_random(self.sleep_select_subtab_city_project)
 
     def _check_all_cities(self):
         start_city_label = get_city_label()
-        start_city_label_filename = 'data/city/start_city_label.png'
+        start_city_label_filename = CITY_FOLDER + "/" + TEMP_CITY_LABEL
         start_city_label.save(start_city_label_filename)
         on_screen = False
         while not on_screen:
@@ -133,10 +148,15 @@ class CityInvest:
             on_screen, position, _ = image_on_screen(start_city_label_filename)
 
     def _donate_if_needed(self):
-        have_contributions, position, _ = image_on_screen(USER_LABEL)
+        user_label = [USER_LABEL, USER_LABEL_SMALL]
+        have_contributions, _, _, _ = any_image_on_screen(user_label)
         if not have_contributions:
-            find_image_and_click([CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE], msg="city contribute")
-            find_image_and_click([CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN], msg="city contribute sub btn")
+            city_contribute_btn = [CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE,
+                                   CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SMALL]
+            find_image_and_click(city_contribute_btn, msg="city contribute")
+            contribute_btn = [CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN,
+                              CITY_SUBTAB_CITY_PROJECT_CONTRIBUTE_SUBBTN_SMALL]
+            find_image_and_click(contribute_btn, msg="city contribute sub btn")
             screenshot_contribute_pop_up = get_screenshot_contribute_pop_up()
             find_image_and_click([GENERAL_BTN_X_CLOSE], screenshot=screenshot_contribute_pop_up,
                                  msg="close city contribute pop-up")
