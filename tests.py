a9e786d72987abe81e0e3a45f3e682615695c226
Jose Soto <josesoto1993@example.com>
2023-06-29 13:02:10 -0300

    refactor test

--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ from time import sleep
 import pyautogui
 
 from association.worker_bid.worker_bid import WorkerBid
-from association.worker_bid.workers import WORKER_ACCELERATION
 from rail_utils.rail_utils import get_screenshot, image_on_screen, beep
 
 logging.root.setLevel(logging.DEBUG)
@@ -20,31 +19,33 @@ def count_down():
     beep()
 
 
+def try_get_bid_amount():
+    count_down()
+    wb = WorkerBid()
+    bid_amount = wb._get_bid_amount()
+    logging.info(f"bid amount: {bid_amount}")
+    is_ok = bid_amount > 0
+    return is_ok
+
+
+def try_get_image():
+    count_down()
+    file_path = "data/general/continue_playing_small.png"
+    screenshot = get_screenshot()
+    screenshot.save("data/screenshot.png")
+    precision = 0.1
+    is_ok, position, _ = image_on_screen(file_path,
+                                         precision=precision,
+                                         screenshot=screenshot,
+                                         gray_scale=True)
+    x, y = position
+    pyautogui.moveTo(x=x, y=y)
+    return is_ok
+
+
 class IsImageOnScreen(unittest.TestCase):
-    def test_test_image(self):
-        count_down()
-
-        file_path = WORKER_ACCELERATION
-        screenshot = get_screenshot()
-        screenshot.save("data/screenshot.png")
-        precision = 0.1
-        is_on_screen, position, _ = image_on_screen(file_path,
-                                                    precision=precision,
-                                                    screenshot=screenshot,
-                                                    gray_scale=True)
-        x, y = position
-        pyautogui.moveTo(x=x, y=y)
-
-        self.assertTrue(is_on_screen)
-
-    def test_get_bid_amount(self):
-        count_down()
-
-        wb = WorkerBid()
-        bid_amount = wb._get_bid_amount()
-        logging.info(f"bid amount: {bid_amount}")
-
-        self.assertTrue(bid_amount > 0)
+    def test(self):
+        self.assertTrue(try_get_image())
 
 
 if __name__ == '__main__':
