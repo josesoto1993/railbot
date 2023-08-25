9e8c4e6b79f8982cbaabda768f3587cf43f3ee7b
Jose Soto <josesoto1993@example.com>
2023-08-25 20:52:41 -0300

    enable all flags aggain as all are working now

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from association.worker_bid.worker_bid import WorkerBid
 from engines.pax_schedule.pax_schedule import PaxSchedule
 from engines.service_engine.service_engine import ServiceEngine
@@ -9,13 +11,15 @@ from redeem.medals.medal_redeem import MedalRedeem
 
 START_PAX_SCHEDULE_MINUTE = 0
 RUN_PAX_SCHEDULE_FLAG = True
-RUN_INDUSTRY_INVEST_FLAG = False # TODO hacer que no quede pegado en filter ya que intenta abrir y cerrar mil veces
-RUN_CITY_INVEST_FLAG = False
+RUN_INDUSTRY_INVEST_FLAG = True
+RUN_CITY_INVEST_FLAG = True
 RUN_SERVICE_ENGINE_FLAG = True
 RUN_WORKER_BID_FLAG = True
 RUN_REDEEM_MEDAL_FLAG = True
 RUN_BUILDING_BONUS_FLAG = False
 BEEP_COUNTDOWN_FLAG = False
+
+logging.root.setLevel(logging.INFO)
 
 
 def main():
@@ -32,9 +36,10 @@ def main():
         tasks.append(ServiceEngine())
     if RUN_WORKER_BID_FLAG:
         tasks.append(WorkerBid())
-        # TODO: puede pasar que no bidee por que agarro mal el numero (piensa es <10k sale alerta dinero insuficiente)
-        # Si sale esa alerta, repetir el loop con tiempo 0, como si fuera un mal investment
+        # TODO: puede pasar que no bidee por que agarro mal el numero (piensa es <10k sale alerta dinero
+        #  insuficiente), Si sale esa alerta, repetir el loop con tiempo 0, como si fuera un mal investment
         # TODO: que entre a descipción, vea si interesa el worker y luego es que skipea, para no re intentar en useless
+        # TODO: que se chequeen TODOS los workers, y tener una lista de los que interesan o algo
     if RUN_REDEEM_MEDAL_FLAG:
         tasks.append(MedalRedeem())
 
